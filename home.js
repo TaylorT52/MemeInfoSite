2bd440b89077289ede24a00bbd8cd871d2f39dc2
Taylor Tam <taylor@taylortam.com>
2022-12-05 07:19:16 -0800

    filled out cards on about page

--- home.js
+++ home.js
@@ -1,10 +1,9 @@
 var counter = 0;
 let timer = setInterval(titleAnim, 300);
-let fullTitle = "ee the history of some of your favorite memes"; 
+let fullTitle = "ee the history of some of your favorite memes!";
 var title = document.getElementById("title");
 
 function titleAnim(){
-    console.log("Hi!");
     var currStr = title.innerHTML; 
     var add = fullTitle.charAt(counter); 
     if(counter > 0){
