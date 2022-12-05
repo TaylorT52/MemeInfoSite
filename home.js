3c6bacfa12b2a3cf87558551277fe03e3ee1d4c1
Taylor Tam <taylor@taylortam.com>
2022-12-04 22:23:33 -0800

    Added title animation

---
+++ home.js
@@ -0,0 +1,20 @@
+var counter = 0;
+let timer = setInterval(titleAnim, 300);
+let fullTitle = "ee the history of some of your favorite memes"; 
+var title = document.getElementById("title");
+
+function titleAnim(){
+    console.log("Hi!");
+    var currStr = title.innerHTML; 
+    var add = fullTitle.charAt(counter); 
+    if(counter > 0){
+        title.innerHTML = title.innerHTML.split("|")[0] + add + "|"; 
+    }else{
+        title.innerHTML = currStr + add + "|"; 
+    }
+    counter += 1; 
+    if(counter > currStr.length){
+        clearInterval(timer); 
+        title.innerHTML = title.innerHTML.split("|")[0]; 
+    }
+}
