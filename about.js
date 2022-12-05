fa7e78eabf38ebb507f0b801d566274386da5692
Taylor Tam <taylor@taylortam.com>
2022-12-04 23:12:58 -0800

    timeline anims

--- about.js
+++ about.js
@@ -1,12 +1,8 @@
 function populateTimeline(){
     var elements = document.getElementsByClassName("timeline-marker");
-    var elemsdiv = document.getElementById("timeline-items"); 
-    elemsdiv.style.transform = "translateX(5vw)"; 
     for(var i = 0; i < elements.length; i++){
         var x = (i+1) * -0.5; 
         var y = -3; 
         elements[i].style.transform = "translate(" + x +"vw, " + y + "vh)";
     }
-    console.log(window.innerHeight); 
-    console.log(elements.length); 
 }
